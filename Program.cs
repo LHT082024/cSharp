c2828935a93863d52a250a1643e61aaa098a7e26
LHT082024 <LHT082024@gmail.com>
2024-09-16 15:28:34 +0200

    task

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace cSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    //     string datatype variabel
+        string helloWorld = "Tom riddle's Diary";
+    
+        //heltall vairabel
+        int myNumber = 10;
+        
+        // command to run the variables in console 
+        Console.WriteLine(myNumber + 10.5); 
+        Console.WriteLine(helloWorld);
+        string? navn = Console.ReadLine();
+        Console.WriteLine("hello " + navn + " My name is Tom Riddle"); 
+
+        // string[] myStringArray = {"hello", "C#", "Array"};
+        // for (int i = 0 ; i < myStringArray.Length; i++)
+        // {
+        //     Console.WriteLine(myStringArray[0]);
+        // }
+        // List<int> myNumbers = new List<int>();
+        // myNumbers.Add(1);
+        // myNumbers.Add(2);
+        // myNumbers.Add(3);
+        // myNumbers.Add(4);
+        // for(int i = 0; i <myNumbers.Count(); i++)
+        // {
+        //     Console.WriteLine(myNumbers[i]);
+        // }
+       
+
+
+    }
+}
+
